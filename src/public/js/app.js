f4e8f36934e455a39dc83e2b4f5d6e39bfc9b8b7
BILW <sunmer6@gmail.com>
2023-04-26 12:50:22 +0900

    websocket을 이용한 양방향 채팅 기능 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,58 +1,58 @@
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message")
-const nickForm = document.querySelector("#nickname")
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('#message');
+const nickForm = document.querySelector('#nickname');
 
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 function makeMessage(type, payload) {
-  const msg = {type, payload}
-  return JSON.stringify(msg)
+  const msg = { type, payload };
+  return JSON.stringify(msg);
 }
 
-socket.addEventListener("open", () => {
-  console.log("Connected to Server");
+socket.addEventListener('open', () => {
+  console.log('Connected to Server');
 });
 
 // message 받기
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  
+socket.addEventListener('message', message => {
+  const li = document.createElement('li');
+
   // 메세지의 데이터가 blob 형식일 경우
   if (message.data instanceof Blob) {
     reader = new FileReader();
-    
+
     reader.onload = () => {
-      console.log("Result: " + reader.result);
-      li.innerText = reader.result
+      console.log('Result: ' + reader.result);
+      li.innerText = reader.result;
     };
     reader.readAsText(message.data);
   } else {
-    console.log("Else Result: " + message.data);
-    li.innerText = message.data
+    console.log('Else Result: ' + message.data);
+    li.innerText = message.data;
   }
-  messageList.append(li)
+  messageList.append(li);
 });
 
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Server");
+socket.addEventListener('close', () => {
+  console.log('Disconnected from Server');
 });
 
-function handleSubmit(event) {
+function handleMsgSubmit(event) {
   event.preventDefault();
-  const input = messageForm.querySelector("input"); // input 가져오기
-  socket.send(makeMessage("new_message", input.value)) // socket을 통해서 backend로 input값을 보냄 
-  input.value = ""
-};
-
+  const input = messageForm.querySelector('input'); // input 가져오기
+  socket.send(makeMessage('new_message', input.value)); // socket을 통해서 backend로 input값을 보냄
+  const li = document.createAttribute;
+  li.innerText = `You: ${input.value}`;
+  messageList.append(li);
+  input.value = '';
+}
 
 function hanldeNickSubmit(event) {
   event.preventDefault();
-  const input = nickForm.querySelector("input")
-  socket.send(makeMessage("nickname", input.value));
-  input.value = ""
-
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+  input.value = '';
 }
 
-
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", hanldeNickSubmit);
+messageForm.addEventListener('submit', handleMsgSubmit);
+nickForm.addEventListener('submit', hanldeNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,51 +1,50 @@
-import express from "express";
-import http from "http";
-import WebSocket from "ws";
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 
 // pugfh view 엔진 설정
-app.set("view engine", "pug");
+app.set('view engine', 'pug');
 
 // express에 templet이 어디에 있는지 알려줌
-app.set("views", __dirname + "/views");
+app.set('views', __dirname + '/views');
 
 // public 설정해서 user에게 경로 지정
-app.use("/public", express.static(__dirname + "/public"));
+app.use('/public', express.static(__dirname + '/public'));
 
 // pug.home 을 rendering 해주는 router
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"))
+app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 
 // websocket 서버만 사용하고 싶으면 ws://localhost:3000 를 사용해도 됨
-const handleListen = () => console.log("Listening on http://localhost:3000");
+const handleListen = () => console.log('Listening on http://localhost:3000');
 // app.listen(3000, handleListen);
-
 
 // websocket server를 http 서버 위에 설정
 // 같은 포트 넘버를 사용하기 위해서 함께 설정함
 const server = http.createServer(app);
-const WebSocketServer = new WebSocket.Server({server});
+const WebSocketServer = new WebSocket.Server({ server });
 
 const sockets = [];
 
-WebSocketServer.on("connection", (socket) => {
-  sockets.push(socket)
-  socket["nickname"] = "Anonymous" // socket에 데이터 저장하기
-  console.log("Connected to Browser");
-  socket.on("close", () => console.log("Desconnected from the Browser"))
+WebSocketServer.on('connection', socket => {
+  sockets.push(socket);
+  socket['nickname'] = 'Anonymous'; // socket에 데이터 저장하기
+  console.log('Connected to Browser');
+  socket.on('close', () => console.log('Desconnected from the Browser'));
 
-  socket.on("message", (message) => {
-    const messageParsed = JSON.parse(message.toString())
+  socket.on('message', message => {
+    const messageParsed = JSON.parse(message.toString());
 
-    switch(messageParsed.type) {
-      case "new_message":
-        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${messageParsed.payload}`))
-        return
-      case "nickname":
-        socket.nickname = messageParsed.payload
+    switch (messageParsed.type) {
+      case 'new_message':
+        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${messageParsed.payload}`));
+        return;
+      case 'nickname':
+        socket.nickname = messageParsed.payload;
     }
   });
 });
 
-server.listen(3000, handleListen)
+server.listen(3000, handleListen);
